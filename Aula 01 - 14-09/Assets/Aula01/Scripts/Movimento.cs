bc41ef31297ab796590fe55e90d53aa3d2572791
Daniel de Oliveira <lacrimaopera@gmail.com>
2016-09-15 23:05:08 -0300

    Primeiro commit

---
+++ Aula 01 - 14-09/Assets/Aula01/Scripts/Movimento.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movimento : MonoBehaviour {
+
+	// Soma da posicao
+	float soma = 0.2f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		// Armazenar a posiçao atual
+		Vector2 posicao = transform.position;
+
+		// Verificar a posiçao
+		if (transform.position.x > 5f ||
+		    transform.position.x < -5f) {
+			soma *= -1;
+		}
+
+		// Mover a sprite
+		transform.position = new Vector2 (
+			posicao.x + soma,
+			posicao.y
+		);
+
+	}
+}
