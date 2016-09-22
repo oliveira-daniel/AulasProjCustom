445e1ff2fe70371fa2d7e53b102808167ec28de3
Daniel de Oliveira <lacrimaopera@gmail.com>
2016-09-21 21:55:20 -0300

    Aula dia 21/09/2016
    Conteúdo visto em aula

---
+++ Aula 01 - 14-09/Assets/ProjCustom/Scripts/MovimentoTeclado.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovimentoTeclado : MonoBehaviour {
+
+	public float velocidade;
+	private Rigidbody2D rb;
+
+	// Use this for initialization
+	void Start () {
+		// Vincular o componente
+		rb = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		// Movimentar usando as fisicas da unity
+		// rb.velocity = Vector2.left * velocidade;
+		rb.velocity = new Vector2(
+			Input.GetAxis("Horizontal") * velocidade,
+			Input.GetAxis("Vertical") * velocidade
+		);
+
+
+
+	}
+}
---
+++ Aula 01 - 14-09/Assets/ProjCustom/Scripts/Player.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	public int pontos;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void AdicionarPontos(int valor) {
+		// Adicionar um ponto a mais ao jogador
+		pontos += valor;
+	}
+}
